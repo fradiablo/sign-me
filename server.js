0780f0e574df5d59b8e1670ffd82d6a7e9fc42f9
Sam <sabramow@tulane.edu>
2017-07-08 14:08:30 -0400

    saving

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require ('mongoose');
 
-var db = mongoose.createConnection('mongodb://localhost/music-library');
+var db = mongoose.connect('mongodb://localhost/music-library');
 
 var Album = require('./model/album');
 var Library = require('./model/library');
@@ -11,15 +11,29 @@ var Library = require('./model/library');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false})); 
 
+app.get('album', function (req, res) {
+    var albumName = req.body.albumName ? req.body.albumName;
+    
+    Album.find({}, function(err, albums) {
+        if (err) {
+            res.status(500).send({error: "Could not fetch products!"});
+        } else {
+            res.send(albums);
+        }
+    });
+});
+
 app.post('/album', function (req, res) {
-    var album = new Album(req.body);
+    var album = new Album();
+    album.title = req.body.title;
+    album.artist = req.body.artist;
     album.save(function(err, savedAlbum) {
         if (err) {
             res.status(500).send({error:"Could not save album!"});
         } else {
-            res.status(200).send(savedAlbum);
+            res.send(savedAlbum);
         }
-    })
+    });
 });
 
 app.listen(3000, function() {
